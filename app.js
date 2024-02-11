ff7abcb50bff2a5e0f67f0f40a26b95cee3c4ed6
7SinCo <marcial.sube757@outlook.com>
2024-02-11 01:01:51 -0600

    create repo encryptor

---
+++ app.js
@@ -0,0 +1,35 @@
+const encryptText = document.querySelector(".user-text");
+const copyText = document.querySelector(".inputCopy");
+let message;
+
+function encrypt() {
+  message = encryptText.value
+    .replaceAll("e", "enter")
+    .replaceAll("i", "imes")
+    .replaceAll("a", "ai")
+    .replaceAll("o", "ober")
+    .replaceAll("u", "ufat");
+  console.log(message);
+
+  copyText.value = message;
+}
+
+function decrypt() {
+  message = encryptText.value
+    .replaceAll("enter", "e")
+    .replaceAll("imes", "i")
+    .replaceAll("ai", "a")
+    .replaceAll("ober", "o")
+    .replaceAll("ufat", "u");
+  console.log(message);
+
+  copyText.value = message;
+}
+
+function copy() {
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+}
