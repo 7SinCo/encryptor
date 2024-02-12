b3e91a46fa19e4cf8e27441892c38ebd38c802cd
7SinCo <marcial.sube757@outlook.com>
2024-02-12 01:32:11 -0600

    update not found

--- app.js
+++ app.js
@@ -1,31 +1,49 @@
+// TextAreas
 const encryptText = document.querySelector(".user-text");
 const copyText = document.querySelector(".inputCopy");
+
+// Result Button
+const textFound = document.querySelector(".text-found");
+const notFound = document.querySelector(".not-found");
+
 let message;
 
 function encrypt() {
-  message = encryptText.value
-    .replaceAll("e", "enter")
-    .replaceAll("i", "imes")
-    .replaceAll("a", "ai")
-    .replaceAll("o", "ober")
-    .replaceAll("u", "ufat");
-  console.log(message);
+  if (encryptText.value) {
+    message = encryptText.value
+      .replaceAll("e", "enter")
+      .replaceAll("i", "imes")
+      .replaceAll("a", "ai")
+      .replaceAll("o", "ober")
+      .replaceAll("u", "ufat");
 
-  copyText.value = message;
-  encryptText.value = "";
+    copyText.value = message;
+    encryptText.value = "";
+    textFound.classList.remove("display-none");
+    notFound.classList.add("display-none");
+  } else {
+    textFound.classList.add("display-none");
+    notFound.classList.remove("display-none");
+  }
 }
 
 function decrypt() {
-  message = encryptText.value
-    .replaceAll("enter", "e")
-    .replaceAll("imes", "i")
-    .replaceAll("ai", "a")
-    .replaceAll("ober", "o")
-    .replaceAll("ufat", "u");
-  console.log(message);
+  if (encryptText.value) {
+    message = encryptText.value
+      .replaceAll("enter", "e")
+      .replaceAll("imes", "i")
+      .replaceAll("ai", "a")
+      .replaceAll("ober", "o")
+      .replaceAll("ufat", "u");
 
-  copyText.value = message;
-  encryptText.value = "";
+    copyText.value = message;
+    encryptText.value = "";
+    textFound.classList.remove("display-none");
+    notFound.classList.add("display-none");
+  } else {
+    textFound.classList.add("display-none");
+    notFound.classList.remove("display-none");
+  }
 }
 
 function copy() {
